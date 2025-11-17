462253cdcb48e2cc422b50a7d9f6b22e6e259853
Xurdeds67 <xurdeds67@educastur.es>
2025-11-17 13:17:13 +0100

    Agregar cambios para UT04_ACT1b

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,71 +1,77 @@
 import React from "react";
-import './Form.css'
+import './Form.css';
 
-class Form extends React.Component{
+function Form(props) {
 
-    envioFormulario=(event)=>{
+    const envioFormulario = (event) => {
         event.preventDefault();
-        const form= event.target;
-        this.props.agregarIncidencia( form.titulo.value, form.usuario.value, form.descripcion.value,
-            form.categoria.value, form.nivel.value, form.ubicacion.value
-        )
-    }
+        const form = event.target;
 
-    render(){
-        return(
-            <div>
-                <h2>Registrar incidencia</h2>
-                <form onSubmit={this.envioFormulario}>
-                    
+        props.agregarIncidencia(
+            form.titulo.value,
+            form.usuario.value,
+            form.descripcion.value,
+            form.categoria.value,
+            form.nivel.value,
+            form.ubicacion.value
+        );
+    };
 
-                    <div className="elemento-form">
-                        <label>Titulo:</label>
-                        <input type="text" name="titulo" placeholder="Introduce el titulo" required></input>
-                    </div>
+    return (
+        <div>
+            <h2>Registrar incidencia</h2>
 
-                    <div className="elemento-form">
-                        <label>Usuario:</label>
-                        <input type="text" name="usuario" placeholder="Introduce el usuario" required></input>
-                    </div>
+            <form onSubmit={envioFormulario}>
 
-                    <div className="elemento-form">
-                        <label>Descripcion:</label>
-                        <input type="text" name="descripcion" placeholder="Introduce la descripcion" required></input>
-                    </div>
+                <div className="elemento-form">
+                    <label>Titulo:</label>
+                    <input type="text" name="titulo" placeholder="Introduce el título" required />
+                </div>
 
-                    <div className="elemento-form">
-                        <label>Categoria:</label>
-                        <select name="categoria" required>
-                            <option value="">Seleccionar...</option>
-                            <option>Hardware</option>
-                            <option>Software</option>
-                            <option>Red y conectividad</option>
-                            <option>Usuarios y accesos</option>
-                            <option>Infraestructuras</option>
-                        </select>
-                    </div>
+                <div className="elemento-form">
+                    <label>Usuario:</label>
+                    <input type="text" name="usuario" placeholder="Introduce el usuario" required />
+                </div>
 
-                    <div className="elemento-form">
-                        <label>Nivel de Urgencia:</label>
-                        <select name="nivel" required>
-                            <option value="">Seleccionar...</option>
-                            <option>Alta</option>
-                            <option>Media</option>
-                            <option>Baja</option>
-                        </select>
-                    </div>
-                    
-                    
+                <div className="elemento-form">
+                    <label>Descripción:</label>
+                    <input type="text" name="descripcion" placeholder="Introduce la descripción" required />
+                </div>
 
-                    <div className="elemento-form">
-                        <label>Ubicacion:</label>
-                        <input type="text" name="ubicacion" placeholder="Ej: B205" required></input>
-                    </div>
+                <div className="elemento-form">
+                    <label>Categoría:</label>
+                    <select name="categoria" required>
+                        <option value="">Seleccionar...</option>
+                        <option>Hardware</option>
+                        <option>Software</option>
+                        <option>Red y conectividad</option>
+                        <option>Usuarios y accesos</option>
+                        <option>Infraestructuras</option>
+                    </select>
+                </div>
 
-                    <button type="submit" className="elemento-form-button">Registrar</button>
-                </form>
-            </div>
-        )
-    }
+                <div className="elemento-form">
+                    <label>Nivel de urgencia:</label>
+                    <select name="nivel" required>
+                        <option value="">Seleccionar...</option>
+                        <option>Alta</option>
+                        <option>Media</option>
+                        <option>Baja</option>
+                    </select>
+                </div>
+
+                <div className="elemento-form">
+                    <label>Ubicación:</label>
+                    <input type="text" name="ubicacion" placeholder="Ej: B205" required />
+                </div>
+
+                <button type="submit" className="elemento-form-button">
+                    Registrar
+                </button>
+
+            </form>
+        </div>
+    );
 }
+
 export default Form;
